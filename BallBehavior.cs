e0f93b65469e20bd7aeb4bfa376b81bb5a1f0dfc
Yaroslav <yar.tarasiuk@gmail.com>
2020-04-25 22:13:58 +0300

    Work on game logic.

---
+++ BallBehavior.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : SphereBehaviour
+{
+    internal int RespIndex { get; set; }            //індекс респавна шара, за замовчуванням 0
+}
